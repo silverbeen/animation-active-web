67c887a8232d6f5e560fc6d9a36304de71310d04
강은빈 <kub0803@naver.com>
2021-02-27 04:53:16 +0900

    메인페이지 스타일 수정

--- src/components/mainPage/Main.tsx
+++ src/components/mainPage/Main.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import * as S from "./style";
 import Header from "./Header";
+import { BiChevronUpCircle, BiChevronDownCircle } from "react-icons/bi";
 
 function Main() {
   return (
     <S.MainWrapper>
       <Header />
-      <a href="https://github.com/silverbeen/Study-TypeScript/tree/master/animation">
-        GIT
-      </a>
+      <S.Main>
+        <div className="introduce">
+          <img src="/" alt="이미지" />
+          <h1>ROTATION</h1>
+        </div>
+        <S.ButtonWrapper>
+          <BiChevronUpCircle size={36} color="white" />
+          <BiChevronDownCircle size={36} color="white" />
+        </S.ButtonWrapper>
+      </S.Main>
     </S.MainWrapper>
   );
 }
--- src/components/mainPage/style.ts
+++ src/components/mainPage/style.ts
@@ -2,16 +2,15 @@ import styled from "styled-components";
 
 const HeaderWrapper = styled.div`
   width: 100%;
-  height: 100px;
-  border: 1px solid red;
 `;
 
 const Nav = styled.nav`
   width: 100%;
-  height: 100%;
-  
+  height: 70px;
+
   i {
-    width: 100%;
+    margin: 0 auto;
+    width: 70%;
     height: 100%;
     display: flex;
     flex-direction: row;
@@ -20,6 +19,7 @@ const Nav = styled.nav`
   }
 
   a {
+    margin: 10px;
     color: white;
     font-size: 20px;
     font-weight: 600;
@@ -30,9 +30,30 @@ const Nav = styled.nav`
 
 //메인페이지
 const MainWrapper = styled.div`
-  width: 100%auto;
+  width: 100%;
   height: 100vh;
-  background-color: #bc5be4;
+  background-color: rgb(240, 195, 49);
 `;
 
-export { HeaderWrapper, Nav, MainWrapper };
+const Main = styled.div`
+  width: 60%;
+  height: 60%;
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+
+  .introduce {
+    display: flex;
+    justify-content: row;
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  margin: 20px;
+  width: 100%;
+  height: 40px;
+  border: 1px solid red;
+`;
+
+export { HeaderWrapper, Nav, MainWrapper, Main, ButtonWrapper };
